484a2f654e4362dd80f7cd97f9d2598b6295659c
luandarezende <luanda.rezende>
2020-10-21 21:15:01 -0300

    send project

---
+++ src/components/Users.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="container">
+    <h3> Users:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.id"> 
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+        </tr>
+      </tbody>
+    </table> 
+    <input type="text">
+    <button @click="adicionar()">Adicionar</button>
+</div> 
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'Users',
+    data() {
+      return {
+        users: null,
+      };
+    },
+    created: function() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+          this.users = res.data;
+        })
+    },
+
+    methods:{
+        adicionar(){
+            
+        let id = 10;
+        this.users.push( {
+            "id": id + 1,
+            "name": "Patricia Lebsack",
+            "username": "Karianne",
+            "email": "Julianne.OConner@kory.org",
+            "address": {
+            "street": "Hoeger Mall",
+            "suite": "Apt. 692",
+            "city": "South Elvis",
+            "zipcode": "53919-4257",
+            "geo": {
+                "lat": "29.4572",
+                "lng": "-164.2990"
+            }
+            },
+            "phone": "493-170-9623 x156",
+            "website": "kale.biz",
+            "company": {
+            "name": "Robel-Corkery",
+            "catchPhrase": "Multi-tiered zero tolerance productivity",
+            "bs": "transition cutting-edge web services"
+            }
+        },);
+        console.log(this.users)
+        }
+    } 
+
+  }
+</script>
+
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
